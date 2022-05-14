afc9edbba79dfe2bd84a6ceb7d3c1ff6a878236f
Robert Gonzalez <robertsgonzalez92@yahoo.com>
2022-05-14 14:29:01 -0500

    questions 4-7 complete

--- EmployeeSQL/EMP_TABLES.sql
+++ EmployeeSQL/EMP_TABLES.sql
@@ -83,6 +83,10 @@ SELECT first_name, last_name, hire_date
 FROM employees 
 WHERE hire_date BETWEEN '1986-1-1' AND '1986-12-31';
 
+SELECT first_name, last_name, hire_date
+FROM employees 
+WHERE CAST (hire_date AS VARCHAR)  LIKE '1986%';
+
 
 -- 3. List the manager of each department with the following information: 
 -- department number, department name, the manager's employee number,
@@ -96,6 +100,42 @@ ON e.emp_no=dm.emp_no;
 
 --4. List the department of each employee with the following information: 
 -- employee number, last name, first name, and department name.
+
+SELECT e.emp_no, e.last_name, e.first_name AS employees, d.dept_name AS departments
+FROM employees e, departments d;
+
+--5. List first name, last name, and sex for employees
+-- whose first name is "Hercules" and last names begin with "B."
+
+SELECT first_name, last_name, sex
+FROM employees 
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
+
+--6. List all employees in the Sales department, including their employee number,
+-- last name, first name, and department name.
+
+SELECT e.emp_no, e.last_name, e.first_name AS employees, d.dept_name AS departments
+FROM employees e, departments d
+WHERE d.dept_name = 'Sales';
+
+--7. List all employees in the Sales and Development departments, 
+-- including their employee number, last name, first name, and department name.
+SELECT e.emp_no, e.last_name, e.first_name AS employees, d.dept_name AS departments
+FROM employees e, departments d
+WHERE d.dept_name = 'Sales' OR d.dept_name = 'Development';
+
+-- List the frequency count of employee last names 
+-- (i.e., how many employees share each last name) in descending order
+
+
+
+
+SELECT * FROM departments
+SELECT * FROM dept_emp
+SELECT * FROM dept_manager
+SELECT * FROM salaries
+SELECT * FROM employees
+SELECT * FROM titles
 
 
 
